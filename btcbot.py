a7d5654859d67ade270701612618982a131b37ad
buttersbtc <76546496+buttersbtc@users.noreply.github.com>
2020-12-23 22:00:44 -0700

    main bot

---
+++ btcbot.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix="!", description="BTC Bot")
+
+@bot.event
+async def on_ready():
+	print('Logged in as {0.user}'.format(bot))
+	print(discord.__version__)
+
+# Loads all commands from the cogs directory
+cogs = [i for i in os.listdir("cogs") if i.endswith(".py")]
+
+for cog in cogs:
+	cog_name = cog.split(".py")[0]
+	bot.load_extension("cogs.{0}".format(cog_name))
+
+# Disables the default help command from discord.py
+bot.remove_command('help')
+# bot.run(username, password, bot=False)
+bot.run(TOKEN)
