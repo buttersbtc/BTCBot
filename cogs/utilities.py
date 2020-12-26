d629d01bc7af1bdeb1ff30b7c75de41d65fb9902
MrRGnome <5963774+MrRGnome@users.noreply.github.com>
2020-12-25 21:17:03 -0700

    huge refactoring (#5)
    the goal of this refactoring is:
    
    enable all possible currencies without a dedicated method for each
    dramatically reduce the codebase size (~66%)
    make it much simpler to add additional calls
    this is accomplished by consolidating price calls inside one function call which queries the api for any provided currency code, silently failing if not found. calls are now defined in two dictionaries, itemDic and currencyFormatDic. To add a new item price or specific formatting for an already support currency simply add a definition to either dictionary.
    
    in addition, several r/bitcoin specific moderation tools and utilities have been added to the utilities.py file. these should be generalized in a future refactoring to enable broader use of this bot.

---
+++ cogs/utilities.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+import requests
+import json
+import os
+
+class Utilities(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+	
+
+	# Report to r/bitcoin mod-log. Subsitute other channel ID's as necessary
+	#todo get channel by ID
+	@commands.command()
+	async def report(self, ctx):
+		for guild in self.bot.guilds:
+			for channel in guild.channels:
+				if channel.name == "mod-log":
+					msg = ctx.author.mention + " reporting: " + ctx.message.content.replace("!report ", "")
+					if ctx.message.reference is not None:
+						reply = await ctx.channel.fetch_message(ctx.message.reference.message_id)
+						msg += " - " + reply.author.mention + ": " + reply.content + " - " + reply.jump_url
+					await channel.send(msg);
+
+		await ctx.message.delete()
+
+
+	@commands.command()
+	async def newuser(self, ctx):
+		if ctx.message.reference is not None:
+			reply = await ctx.channel.fetch_message(ctx.message.reference.message_id)
+			user = ", " + reply.author.mention
+		else:
+			user = ""
+		await ctx.channel.send("Welcome to the r/Bitcoin chat" + user + ". Please review the #rules while you're here; primarily no altcoin, stock, or off topic discussion. Also please read our newcomers faq at https://www.reddit.com/r/Bitcoin/comments/i19uta/bitcoin_newcomers_faq_please_read/. For additional learning resources and information please check out https://lopp.net/bitcoin.html, a community curated resource list. To report users for breaking these rules please reply to the rulebreaking comment and type !report <reason>.")
+
+def setup(bot):
+	bot.add_cog(Utilities(bot))
