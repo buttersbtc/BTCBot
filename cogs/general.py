04cdce91ba906d7046f3315ce9ec7687c7774164
MrRGnome <5963774+MrRGnome@users.noreply.github.com>
2020-12-25 21:17:03 -0700

    huge refactoring (#5)
    the goal of this refactoring is:
    
    enable all possible currencies without a dedicated method for each
    dramatically reduce the codebase size (~66%)
    make it much simpler to add additional calls
    this is accomplished by consolidating price calls inside one function call which queries the api for any provided currency code, silently failing if not found. calls are now defined in two dictionaries, itemDic and currencyFormatDic. To add a new item price or specific formatting for an already support currency simply add a definition to either dictionary.
    
    in addition, several r/bitcoin specific moderation tools and utilities have been added to the utilities.py file. these should be generalized in a future refactoring to enable broader use of this bot.

--- cogs/general.py
+++ cogs/general.py
@@ -11,201 +11,106 @@ class General(commands.Cog):
 	async def on_ready(self):
 		print("General commands loaded")
 
-
-	# Fetch Bitcoin price
-	@commands.command()
-	async def btc(self, ctx):
-		api = "http://preev.com/pulse/units:btc+usd/sources:bitstamp+kraken"
-
-		r = requests.get(api)
-		data = json.loads(r.text)
-
-		price = data["btc"]["usd"]["bitstamp"]["last"]
-		price = round(float(price),2)
-		price = "${:,.2f} USD".format(float(price))
-		message_string = "**1 Bitcoin** is worth **" + price + "**"		
-		await ctx.send(message_string)
-	
-	# price also fetches USD
+	# Price - All currencies enabled by the APi are automatically supported. Add a currency formatting string to change the way a given currency is displayed
+	#To add a new item to the price call make a new entry in the itemDic with the cost and formatStr, the key being the string  used to call that item
 	@commands.command()
 	async def price(self, ctx, arg="noargs"):
-		api = "http://preev.com/pulse/units:btc+usd/sources:bitstamp+kraken"
+		#Price string details
+		currencyFormatDic = {
+			"default":"${:,.2f} ",
+			"gbp":"£{:,.2f} GBP",
+			"eur":"€{:,.2f} EUR",
+			"brl":"R${:,.2f} BRL",
+			"vef":"B${:,.0f} Venezuelan Bolívar",
+			"jpy":"¥{:,.0f} JPY",
+			"cny":"¥{:,.0f} CNY",
+			"ils":"₪{:,.0f} ILS",
+			"inr":"₹{:,.2f} INR",
+			"zar":"R{:,.2f} ZAR",
+			"rub":"₽{:,.2f} RUB"
+			}
+		itemDic = {
+			"mac": {"cost": 5.71, "formatStr":"**1 Bitcoin** is worth **:hamburger: {:.2f} Big Macs**"},
+			"mcr": {"cost": 4.29, "formatStr":"**1 Bitcoin** is worth **:pig2: {:.2f} McRibs**"},
+			"cru": {"cost": 2.99, "formatStr":"**1 Bitcoin** is worth **:taco: {:.2f} Crunchwraps Supreme**"},
+			"but": {"cost": 0.5, "formatStr":"**1 Bitcoin** is worth **:butter: {:.2f} Sticks of Butter**"},
+			"lam": {"cost": 521465, "formatStr":"**:race_car: 1 Lamborghini Aventador SVJ** costs **{:.2f} Bitcoin**"},
+			"coldcards": {"cost": 119.27, "formatStr":"**1 Bitcoin** is worth **{:.2f} Coldcards"}
+			}
+
 		arg = arg.lower()
+
+		#route to other func if exists
 		func = getattr(self, arg , None)
 		if(callable(func)):
 			await func(ctx)
 			return
 
-		r = requests.get(api)
-		data = json.loads(r.text)
+		#handle items first, fallback to currencies
+		isItem = arg in itemDic
+		item = ""
+		if isItem:
+			item = arg
 
-		price = data["btc"]["usd"]["bitstamp"]["last"]
-		price = round(float(price),2)
-		price = "${:,.2f} USD".format(float(price))
-		message_string = "**1 Bitcoin** is worth **" + price + "**"		
-		await ctx.send(message_string)
-
-	# usd fetches USD
-	@commands.command()
-	async def usd(self, ctx):
-		api = "http://preev.com/pulse/units:btc+usd/sources:bitstamp+kraken"
-
-		r = requests.get(api)
-		data = json.loads(r.text)
-
-		price = data["btc"]["usd"]["bitstamp"]["last"]
-		price = round(float(price),2)
-		price = "${:,.2f} USD".format(float(price))
-		message_string = "**1 Bitcoin** is worth **" + price + "**"		
-		await ctx.send(message_string)
+		if arg == "noargs" or isItem:
+			arg = "usd"
 		
-	# Fetches price in CAD
-	@commands.command()
-	async def cad(self, ctx):
-		api = "http://preev.com/pulse/units:btc+cad/sources:bitstamp+kraken"
-
-		r = requests.get(api)
-		data = json.loads(r.text)
-
-		price = data["btc"]["usd"]["bitstamp"]["last"]
-		conversion = data["cad"]["usd"]["other"]["last"]
-		price = float(price)/float(conversion)
-		price = round(float(price),2)
-		price = "${:,.2f} CAD".format(float(price))
-		message_string = "**1 Bitcoin** is worth **" + price + "**"
-		await ctx.send(message_string)	
-
-	# Fetches price in GBP
-	@commands.command()
-	async def gbp(self, ctx):
-		api = "http://preev.com/pulse/units:btc+gbp/sources:bitstamp+kraken"
-
-		r = requests.get(api)
-		data = json.loads(r.text)
+		try:
+			api = "http://preev.com/pulse/units:btc+" + arg + "/sources:bitstamp+kraken"
+			r = requests.get(api)
+			data = json.loads(r.text)
+		except:
+			return
 
 		price = data["btc"]["usd"]["bitstamp"]["last"]
-		conversion = data["gbp"]["usd"]["other"]["last"]
-		price = float(price)/float(conversion)
-		price = round(float(price),2)
-		price = "£{:,.2f} GBP".format(float(price))
-		message_string = "**1 Bitcoin** is worth **" + price + "**"
-		await ctx.send(message_string)	
+		if arg != "usd":
+			conversion = data[arg]["usd"]["other"]["last"]
+			price = float(price)/float(conversion)
+		if isItem:
+			if itemDic[item]["cost"] < float(price):
+				price = float(price)/itemDic[item]["cost"]
+			else:
+				price = itemDic[item]["cost"]/float(price)
 
-	# Fetches price in EUR
-	@commands.command()
-	async def eur(self, ctx):
-		api = "http://preev.com/pulse/units:btc+eur/sources:bitstamp+kraken"
-
-		r = requests.get(api)
-		data = json.loads(r.text)
-
-		price = data["btc"]["eur"]["kraken"]["last"]
-		price = round(float(price),2)
-		price = "€{:,.2f} EUR".format(float(price))
-		message_string = "**1 Bitcoin** is worth **" + price + "**"
-		await ctx.send(message_string)
+		currencyStr = ""
+		if arg in currencyFormatDic:
+			currencyStr = currencyFormatDic[arg]
+		elif isItem:
+			currencyStr = itemDic[item]["formatStr"]
+		else:
+			currencyStr = currencyFormatDic["default"] + arg.upper()
 		
-	# Fetches price in BRL
-	@commands.command()
-	async def brl(self, ctx):
-		api = "http://preev.com/pulse/units:btc+brl/sources:bitstamp+kraken"
-
-		r = requests.get(api)
-		data = json.loads(r.text)
-
-		price = data["btc"]["usd"]["bitstamp"]["last"]
-		conversion = data["brl"]["usd"]["other"]["last"]
-		price = float(price)/float(conversion)
-		price = round(float(price),2)
-		price = "R${:,.2f} BRL".format(float(price))
-		message_string = "**1 Bitcoin** is worth **" + price + "**"
-		await ctx.send(message_string)	
-
-	# Fetches price in VEF
-	@commands.command()
-	async def vef(self, ctx):
-		api = "http://preev.com/pulse/units:btc+vef/sources:bitstamp+kraken"
-
-		r = requests.get(api)
-		data = json.loads(r.text)
-
-		price = data["btc"]["usd"]["bitstamp"]["last"]
-		conversion = data["vef"]["usd"]["other"]["last"]
-		price = float(price)/float(conversion)
-		price = round(float(price))
-		price = "B${:,.0f} Venezuelan bolívar".format(float(price))
-		message_string = "**1 Bitcoin** is worth **" + price + "**"
-		await ctx.send(message_string)	
-	
-	# Fetches price in BigMacs
-	@commands.command()
-	async def mac(self, ctx):
-		api = "http://preev.com/pulse/units:btc+usd/sources:bitstamp+kraken"
-
-		r = requests.get(api)
-		data = json.loads(r.text)
-
-		price = data["btc"]["usd"]["bitstamp"]["last"]
-		price = round(float(price)/5.71)
-		price = ":hamburger: {:,} Big Macs".format(price)
-		message_string = "**1 Bitcoin** is worth **" + price + "**"	
-		await ctx.send(message_string)	
-
-	# Fetches price in McRibs
-	@commands.command()
-	async def mcr(self, ctx):
-		api = "http://preev.com/pulse/units:btc+usd/sources:bitstamp+kraken"
-
-		r = requests.get(api)
-		data = json.loads(r.text)
-
-		price = data["btc"]["usd"]["bitstamp"]["last"]
-		price = round(float(price)/4.29)
-		price = ":pig2: {:,} McRibs".format(price)
-		message_string = "**1 Bitcoin** is worth **" + price + "**"	
-		await ctx.send(message_string)	
-
-	# Fetches price in Crunchwraps
-	@commands.command()
-	async def cru(self, ctx):
-		api = "http://preev.com/pulse/units:btc+usd/sources:bitstamp+kraken"
-
-		r = requests.get(api)
-		data = json.loads(r.text)
-
-		price = data["btc"]["usd"]["bitstamp"]["last"]
-		price = round(float(price)/2.99)
-		price = ":taco: {:,} Crunchwraps Supreme".format(price)
-		message_string = "**1 Bitcoin** is worth **" + price + "**"	
-		await ctx.send(message_string)	
-	
-	# Fetches price in Sticks of Butter
-	@commands.command()
-	async def but(self, ctx):
-		api = "http://preev.com/pulse/units:btc+usd/sources:bitstamp+kraken"
-
-		r = requests.get(api)
-		data = json.loads(r.text)
-
-		price = data["btc"]["usd"]["bitstamp"]["last"]
-		price = round(float(price)/.5)
-		price = ":butter: {:,} Sticks of Butter".format(price)
-		message_string = "**1 Bitcoin** is worth **" + price + "**"	
-		await ctx.send(message_string)	
-
-	# Fetches price in Lambos
-	@commands.command()
-	async def lam(self, ctx):
-		api = "http://preev.com/pulse/units:btc+usd/sources:bitstamp+kraken"
-
-		r = requests.get(api)
-		data = json.loads(r.text)
-
-		price = data["btc"]["usd"]["bitstamp"]["last"]
-		price = round(521465/float(price),8)
-		price = "{:,}".format(price)
-		message_string = "**:race_car: 1 Lamborghini Aventador SVJ** costs **" + price + " BTC**"		
+		price = currencyStr.format(float(price))
+		message_string = ""
+		if isItem:
+			message_string = price
+		else:
+			message_string = "**1 Bitcoin** is worth **" + price + "**"
+		
 		await ctx.send(message_string)
+
+	# Bitcoin is a btc
+	@commands.command()
+	async def btc(self, ctx):
+		message_string = "**1 Bitcoin** is worth **1 Bitcoin**"
+		await ctx.send(message_string)
+	
+
+	# Fetches price in cats
+	@commands.command()
+	async def cat(self, ctx):
+		message_string = "**:black_cat: stop trying to price cats!"
+		await ctx.send(message_string)
+
+	str = 0
+	# Fetches price in Strong
+	@commands.command()
+	async def strong(self, ctx):
+		str=str+1
+		if str % 25 == 0:
+			await ctx.send("oh. that guy.")
+		else:
+			await ctx.send("who?")
 
 def setup(bot):
 	bot.add_cog(General(bot))
