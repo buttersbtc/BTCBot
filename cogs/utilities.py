4a6a7f343eac520eff5e2a9030250a839cbb98ba
MrRGnome <5963774+MrRGnome@users.noreply.github.com>
2021-03-30 15:06:07 -0600

    chart command

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -149,6 +149,17 @@ class Utilities(commands.Cog):
 		msg = "A general purpose and stable high qubit quantum computer (which doesn't exist and no one is sure if will ever exist) can run an algorithm called shor's. Shor's is used to factor numbers.  You can thus use shor's to derive a private key from a public key. Bitcoin exposes public keys in the scenarios of certain address reuse and when certain transactions are sitting in the mempool, as well as very old 2009 era pay to pubkey coinbases. What will happen if such a computer ever exists is slowly attempts to mine the most static of these coins, probably the old coinbases, will occur. Once this happens everyone will know there is a quantum actor and avoid address reuse or in the worst case just move to a new address format. It's also important to remember that a quantum attack takes considerable time, not dissimilar to mining, as it's the process for searching for a private key."
 		await ctx.channel.send(msg)
 
+	@commands.command()
+	async def chart(self, ctx, *args):
+		exchange = args[0].lower() if len(args) > 0 else "bitstamp"
+		timespan = args[0].lower() if len(args) > 1 else "150"
+		r = requests.get("https://bitcoincharts.com/charts/chart.png?width=940&m=" + exchange + "USD&SubmitButton=Draw&r=" + timespan + "&i=&c=0&s=&e=&Prev=&Next=&t=S&b=&a1=&m1=10&a2=&m2=25&x=0&i1=&i2=&i3=&i4=&v=1&cv=0&ps=0&l=0&p=0&", stream = True)
+		if r.status_code == 200:
+			r.raw.decode_content = True
+			embed = discord.Embed()
+			embed.set_image(url="https://bitcoincharts.com/charts/chart.png?width=940&m=" + exchange + "USD&SubmitButton=Draw&r=" + timespan + "&i=&c=0&s=&e=&Prev=&Next=&t=S&b=&a1=&m1=10&a2=&m2=25&x=0&i1=&i2=&i3=&i4=&v=1&cv=0&ps=0&l=0&p=0&")
+			msg = "https://bitcoincharts.com/charts/chart.png?width=940&m=" + exchange + "USD&SubmitButton=Draw&r=" + timespan + "&i=&c=0&s=&e=&Prev=&Next=&t=S&b=&a1=&m1=10&a2=&m2=25&x=0&i1=&i2=&i3=&i4=&v=1&cv=0&ps=0&l=0&p=0&";
+			await ctx.channel.send(embed=embed)
 		
 
 def setup(bot):
