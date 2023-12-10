7a9f0d500da7e63fa068b5c6183fc21e480b6844
QuietNode <152983965+QuietNode@users.noreply.github.com>
2023-12-10 05:18:56 +0100

    Refactor Codebase and Introduce BitcoinAPI for Improved Modularity (#40)
    * Move constants to seperate module.
    
    * Add BitcoinAPI class for fetching Bitcoin data
    
    - Implement get_current_price method to fetch Bitcoin price
    - Add get_currency_rates method for currency exchange rates
    - Include error handling for API requests
    
    * Refactor pricewatch for clarity and robustness
    
    - Moved Bitcoin API calls to a separate class for cleaner code.
    - Improved error handling with specific checks and exception handling.
    - Cleaned up the watch method for better readability.
    - Replaced while(1) with while True for clarity.
    
    * Remove unused imports and sort according to pep 8
    
    * Format file according to pep 8
    
    * Add logging of various events.
    
    * Add return for currency symbol.
    
    * Add docstring for methods.
    
    * Add timeout for GET requests.
    
    * Fix runtime error.
    
    The param timeout for requests.get was not specified correctly
    causing a runtime error.
    
    * Refactor price commands for improved modularity
    
    The !price command and its alias !p were previously handling multiple
    tasks within a single function. This commit breaks down these tasks
    into separate methods, each responsible for a specific subtask such
    as pricing in satoshis, handling item pricing,
    and dealing with various currencies.
    
    These new methods utilize the BitcoinAPI,
    enhancing consistency and integration.
    The refactoring improves maintainability and simplifies
    the process of adding new pricing cases in the future.

--- cogs/general.py
+++ cogs/general.py
@@ -1,3 +1,5 @@
+from BitcoinAPI import BitcoinAPI
+from constants import ITEM_DICT, CURRENCY_FORMAT_DICT, BLACKLIST
 from operator import truediv
 import os
 import discord
@@ -20,202 +22,63 @@ class General(commands.Cog):
 	# Price - All currencies enabled by the APi are automatically supported. Add a currency formatting string to change the way a given currency is displayed
 	#To add a new item to the price call make a new entry in the itemDic with the cost and formatStr, the key being the string  used to call that item
 	@commands.command()
-	async def price(self, ctx, *args):
-		#Price string details
-		currencyFormatDic = {
-			"default":"${:,.2f} ",
-			"gbp":"£{:,.2f} British Pounds",
-			"eur":"€{:,.2f} Euros",
-			"brl":"R${:,.2f} Brazillian Reais",
-			"vef":"B${:,.0f} Venezuelan Bolívar",
-			"jpy":"¥{:,.0f} Japanese Yen",
-			"cny":"¥{:,.0f} Chinese Renminbi",
-			"ils":"₪{:,.0f} Israeli Shekalim",
-			"inr":"₹{:,.2f} Indian Rupees",
-			"zar":"R{:,.2f} South African Rands",
-			"rub":"₽{:,.2f} Russian Rubles",
-			"xau":"{:,.2f} ounces of gold",
-			"xag":"{:,.2f} ounces of silver"
-			}
-		itemDic = {
-			"mac":{"cost": 5.71,"name":"Big Macs","emoji":":hamburger:","single":False},
-			"mcr":{"cost": 4.29,"name":"McRibs","emoji":":pig2:","single":False},
-			"cru":{"cost": 2.99,"name":"Crunchwraps Supreme","emoji":":taco:","single":False},
-			"beer":{"cost": 4.75,"name":"Pints of Beer","emoji":":beer:","single":False},
-			"but":{"cost": .75,"name":"Sticks of Butter","emoji":":butter:","single":False},
-			"coldcards":{"cost": 119.27,"name":"Coldcards","emoji":":pager:","single":False},
-			"egg":{"cost": 0.1208333,"name":"Large Eggs","emoji":":egg:","single":False},
-			"420":{"cost": 200,"name":"Ounces of Marijuana","emoji":":maple_leaf:","single":False},
-			"gum":{"cost": 8.37,"name":"Kilograms of Gummie Bears","emoji":":teddy_bear:","single":False},
-			"rbx":{"cost": 0.0125,"name":"Robux","emoji":":bricks:","single":False},
-			"thc":{"cost": 40,"name":"THC distillate cartridges (1 gram)","emoji":":maple_leaf:","single":False},
-			"pod":{"cost": 5.2475,"name":"JUUL pods","emoji":":smoking:","single":False},
-			"furby":{"cost": 300,"name":"Rare Furbies", "emoji":":owl:","single":False},
-			"avo":{"cost": 10,"name":"Serves of Avocado Toast","emoji":":avocado:","single":False},
-			"chicken":{"cost": 2.85,"name":"Rhode Island Red Chickens","emoji":":chicken:","single":False},
-			"nana":{"cost": 0.23,"name":"Bananas","emoji":":banana:","single":False},
-			"bre":{"cost": 2.04,"name":"Loaves of bread","emoji":":bread:","single":False},
+	async def price(self, ctx, arg):
+		if arg is None:
+			await ctx.send('Invalid price command passed...')
 
-			# Seperating out items that should be displayed as cost for a single item
-			"lam":{"cost": 521465,"name":"Lamborghini Aventador SVJ","emoji":":race_car:","single":True},
-			"act":{"cost": 32410,"name":"Average College Tuition (4 years)","emoji":":student:","single":True},
-			"lar":{"cost": 259000,"name":"McLaren 600LT 2020","emoji":":race_car:","single":True},
-			"tm3":{"cost": 36990,"name":"Tesla Model 3","emoji":":red_car:","single":True},
-			"rds":{"cost": 200000,"name":"Tesla Roadster 2020","emoji":":race_car:","single":True},
-			"f40":{"cost": 1350000,"name":"Ferrari F40","emoji":":race_car:","single":True},
-			"tay":{"cost": 232904,"name":"Porche Taycan Turbo S","emoji":":red_car:","single":True},
-			"mus":{"cost": 75000,"name":"Ford Mustang Shelby GT500 2020","emoji":":blue_car:","single":True},
-			"fc9":{"cost": 62000000,"name":"SpaceX Falcon 9 Launch","emoji":":rocket:","single":True},
-			"trn":{"cost": 139900,"name":"Audi RS e-tron GT 2022","emoji":":race_car:","single":True},
-			"bug":{"cost": 2990000,"name":"Bugatti Chiron 2020","emoji":":race_car:","single":True},
-			"nev":{"cost": 2440000,"name":"Rimac Nevera","emoji":":race_car:","single":True},
-			"gef":{"cost": 1499,"name":"Nvidia GEFORCE RTX 3090","emoji":":desktop_computer:","single":True},
-			"rov":{"cost": 2725000000,"name":"trip to Mars + rover/drone/skycrane package","emoji":":robot: :rocket:","single":True},
-			"bez":{"cost": 74598,"name":"One minute of Jeff Bezos' time","emoji":":man_office_worker:","single":True},
-			"kid":{"cost": 200000,"name":"black market kidney","emoji":":detective: :aubergine:","single":True},
-			"ukb":{"cost": 850000000000,"name":"British banking bailout","emoji":":flag_gb: :bank:","single":True}
-			}
-		blacklist = {
-			"xdg":True,
-			"ltc":True,
-			"eth":True,
-			"xrp":True,
-			"bch":True
-			}
+		arg = arg.lower()
 
-		if len(args) == 0:
-			arg="noargs"
-		else:
-			arg = args[0].lower()
-
-		if len(args) >= 2:
-			arg = args[0].lower()
-			arg2 = args[1].lower()
-		else:
-			arg2 = "noarg"
-
-		if arg in blacklist:
+		if arg in CURRENCY_FORMAT_DICT:
+			await self.price_currency(ctx, arg)
 			return
-
-		if arg =="sats":
-			await ctx.channel.send("**1 Bitcoin** is equal to **100,000,000 Satoshis**")
+		if arg in ITEM_DICT:
+			await self.price_item(ctx, arg)
 			return
-			
-		if arg == "help":
-			await ctx.channel.send("**Currency Eamples**: !p gbp, !p cad, !p xau")
-			keys = ""
-			for k in itemDic.keys():
-				keys += k + ", "
-			keys = keys[0:len(keys)-2]
-			await ctx.channel.send("**Other Supported Items**: " + keys)
-			await ctx.channel.send("**!p <item> sats** will give you the cost of the item in satoshis")
+		if arg == "sats":
+			await self.price_sats(ctx, arg)
 			return
+		return await ctx.send('Invalid price command passed...')
 
-		#route to other func if exists
-		func = getattr(self, arg , None)
-		if(callable(func)):
-			await func(ctx)
+	async def price_sats(self, ctx, *args):
+		await ctx.channel.send("**1 Bitcoin** is equal to **100,000,000 Satoshis**")
+
+	async def price_currency(self, ctx, currency):
+		if currency not in CURRENCY_FORMAT_DICT:
+			await ctx.channel.send("Currency not supported")
 			return
-
-		#handle items first, fallback to currencies
-		isItem = arg in itemDic
-		item = ""
-		if isItem:
-			item = arg
-
-		if arg == "noargs" or isItem:
-			arg = "usd"
-		#call the api for our currency
-		try:
-			api = "https://api.coincap.io/v2/assets/bitcoin"
-			r = requests.get(api, timeout=10)
-			data = json.loads(r.text)
-
-			api_rates = "https://api.coincap.io/v2/rates/"
-			r_rates = requests.get(api_rates, timeout=10)
-			data_rates = json.loads(r_rates.text)
-
-		except:
-			await ctx.send("Price APi is currently slow to respond. Try again later.")
+		api = BitcoinAPI()
+		price, error = api.get_current_price()
+		if error:
+			await ctx.channel.send("Price API is currently slow to respond. Try again later.")
 			return
+		price, _, _ = api.get_current_price_in_currency(currency)
+		price = CURRENCY_FORMAT_DICT[currency].format(price)
+		await ctx.channel.send(price)
 
-		price = data["data"]["priceUsd"]
-		skipConvert = False
-
-		#convert if necessary or do item calcs.
-		found_currency = False
-
-		if arg != "usd" and not skipConvert:
-		
-			for i in data_rates["data"]:
-				if i["symbol"] == arg.upper():
-					found_currency = True
-					price = float(price) / float(i["rateUsd"])
-		
-		if found_currency == False:
-			arg = "USD"
-		
-		if isItem:
-			if itemDic[item]["single"] == False:
-				if itemDic[item]["cost"] < float(price) and arg2=="sats":
-					price = itemDic[item]["cost"]/float(price) * 100000000
-				else:
-					price = float(price)/itemDic[item]["cost"]
-					if arg2 == "sats":
-						price = price * 100000000
-
-			else:
-				price = itemDic[item]["cost"]/float(price)
-				if arg2 == "sats":
-					price = price * 100000000
+	async def price_item(self, ctx, item):
+		if not item in ITEM_DICT:
+			await ctx.channel.send("Item not supported")
+			return
+		api = BitcoinAPI()
+		price, error = api.get_current_price()
+		if error:
+			await ctx.channel.send("Price API is currently slow to respond. Try again later.")
+			return
+		item_map = ITEM_DICT[item]
+		emoji = item_map["emoji"]
+		name = item_map["name"]
+		if item_map["single"]:
+			price = item_map["cost"] / price
+			await ctx.channel.send(f"{emoji} {name} costs {price:,.2f} Bitcoin")
 		else:
-			if arg2 == "sats":
-				price = 100000000/float(price)
-
-
-
-		currencyStr = ""
-		if arg in currencyFormatDic:
-			if arg2 == "sats":
-				currencyStr = "**1 " + arg.upper()  + "** is **" + "{:,.0f} Satoshis**"
-			else:
-				currencyStr = currencyFormatDic[arg]
-
-		elif isItem:
-			if itemDic[item]["single"] == False:
-				if arg2 == "sats":
-					currencyStr = "**" + itemDic[item]["emoji"] + " 1 " + itemDic[item]["name"] + "**" + " costs **" + "{:,.0f}" + " Satoshis**"
-				else:
-					currencyStr = "**1 Bitcoin** is worth **" + itemDic[item]["emoji"] + " {:,.0f}" + " " + itemDic[item]["name"] + "**"
-			else:
-				if arg2 == "sats":
-					currencyStr = "**" + itemDic[item]["emoji"] + " 1 " + itemDic[item]["name"] + "**" + " costs **" + "{:,.0f}" + " Satoshis**"
-				else:
-					currencyStr = "**" + itemDic[item]["emoji"] + " 1 " + itemDic[item]["name"] + "**" + " costs **" + "{:,.2f}" + " Bitcoin**"
-		else:
-			if arg2 == "sats":
-				currencyStr = "**1 " + arg.upper()  + "** is **" + "{:,.0f} Satoshis**"
-			else:
-				currencyStr = currencyFormatDic["default"] + arg.upper()
-
-		
-		price = currencyStr.format(float(price))
-		
-		message_string = ""
-		if isItem:
-			message_string = price
-		else:
-			if arg2 == "sats":
-				message_string = price
-			else:	
-				message_string = "**1 Bitcoin** is worth **" + price + "**"
-		
-		await ctx.send(message_string)
+			price = price / ITEM_DICT[item]["cost"]
+			await ctx.channel.send(f"1 Bitcoin is worth {emoji} {price:,.2f} {name}")
+			return
 
 	# price synonym
 	@commands.command()
-	async def p(self, ctx, *args):
-		await General(self).price(self, ctx, *args)
+	async def p(self, ctx, arg):
+		await General(self).price(self, ctx, arg)
 
 	# Bitcoin is a btc
 	@commands.command()
