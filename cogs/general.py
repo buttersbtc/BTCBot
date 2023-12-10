51f04a76df5ea550e0f3134cbffb9b1716ff2aee
MrRGnome <5963774+MrRGnome@users.noreply.github.com>
2023-12-09 23:25:47 -0700

    help command fix

--- cogs/general.py
+++ cogs/general.py
@@ -226,8 +226,8 @@ class General(commands.Cog):
 		for cmd in sorted_cmd:
 			message_string +=  " " + os.getenv('BOT_PREFIX') + f"{cmd},"
 		message_string = message_string[:len(message_string)-1]
-		message_string += ". For bot support inquire at <http://bitcointech.help> or in the issues at <https://github.com/buttersbtc/BTCBot/issues\>"
-		await ctx.send(message_string[:len(message_string)-1])
+		message_string += ". For bot support inquire at <http://bitcointech.help> or in the issues at <https://github.com/buttersbtc/BTCBot/issues>"
+		await ctx.send(message_string)
 
 async def setup(bot):
 	await bot.add_cog(General(bot))
