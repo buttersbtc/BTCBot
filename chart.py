5e2318460a617fc8b0d5b516b72b91c5c33d9ac0
MrRGnome <5963774+MrRGnome@users.noreply.github.com>
2023-12-24 20:24:49 -0700

    chart formatting

--- chart.py
+++ chart.py
@@ -5,12 +5,21 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as pyplot
 
 def chart(api_response):
-	x = [datetime.datetime.utcfromtimestamp(value['x']) for value in api_response['values']]
-	y = [value['y'] for value in api_response['values']]
+	multiplier = 1
+	if api_response['unit'] == "USD":
+		api_response['unit'] = "Thousands of USD"
+		multiplier = 0.001
+	if api_response['unit'] == "Bytes":
+		api_response['unit'] = "Megabytes"
+		multiplier = 0.000001
 	pyplot.title(api_response['name'])
 	pyplot.ylabel(api_response['unit'])
 	pyplot.xlabel(api_response['period'])
-	pyplot.plot_date(x, y, linestyle='-')
+	x = [datetime.datetime.fromtimestamp(value['x']).strftime("%d %b %y") for value in api_response['values']]
+	y = [(float(value['y']) * multiplier) for value in api_response['values']]
+	pyplot.plot_date(x, y, linestyle='-', markersize=0.0)
+	pyplot.xticks(rotation=45)
+	pyplot.subplots_adjust(bottom=0.20)
 	buf = io.BytesIO()
 	pyplot.savefig(buf, format='png')
 	buf.seek(0)
